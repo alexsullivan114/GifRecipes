50733ff2e6a02bd39fbc8de0aa8d37dee80db25e
Alex Sullivan <Alex Sullivan>
2017-05-08 16:44:40 -0400

    Initial commit

---
+++ app/src/main/java/alexsullivan/gifrecipes/RecipesActivity.java
@@ -0,0 +1,13 @@
+package alexsullivan.gifrecipes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RecipesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipes);
+    }
+}
