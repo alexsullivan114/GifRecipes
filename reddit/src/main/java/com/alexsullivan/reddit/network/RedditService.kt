75cfc8a8ce43e098b189b9252a9f7d8df37b0513
Alex Sullivan <Alex.sullivan@runkeeper.com>
2017-08-10 12:53:12 -0400

    Bug fix - remove ampersand special character

--- reddit/src/main/java/com/alexsullivan/reddit/network/RedditService.kt
+++ reddit/src/main/java/com/alexsullivan/reddit/network/RedditService.kt
@@ -19,5 +19,5 @@ internal interface RedditService {
     @GET("r/gifrecipes/search")
     fun searchRecipes(@Query("q") searchParam: String, @Query("after") after: String? = "",
                       @Query("limit") limit: Int = 100, @Query("restrict_sr") restrict: Boolean? = true,
-                      @Query("sort") sort: String = "hot"): Observable<RedditListingResponse>
+                      @Query("sort") sort: String = "hot", @Query("raw_json") useRawJson: Int = 1): Observable<RedditListingResponse>
 }
