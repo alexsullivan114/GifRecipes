6c1bdbf6097a5c8fd313c748402e47d868f7a28c
Alex Sullivan <Alex.sullivan@runkeeper.com>
2017-08-28 11:01:59 -0400

    Fixed a potential crash on imgur post deserialization.

--- imgur-core/src/main/java/com/gfycat/ImgurPostDeserializer.kt
+++ imgur-core/src/main/java/com/gfycat/ImgurPostDeserializer.kt
@@ -3,11 +3,19 @@ package com.gfycat
 import com.google.gson.JsonDeserializationContext
 import com.google.gson.JsonDeserializer
 import com.google.gson.JsonElement
+import com.google.gson.JsonParseException
 import java.lang.reflect.Type
 
 class ImgurPostDeserializer: JsonDeserializer<ImgurPost> {
     override fun deserialize(json: JsonElement, typeOfT: Type?, context: JsonDeserializationContext): ImgurPost {
         val post = json.asJsonObject.getAsJsonObject("data")
+        if (!post.has("mp4")) {
+            throw JsonParseException("Imgur post does not have mp4 link")
+        }
+
+        if (!post.has("link")) {
+            throw JsonParseException("Imgur post does not have link")
+        }
         return ImgurPost(post.get("mp4").asString, post.get("link").asString)
     }
 }
--- reddit/src/main/java/com/alexsullivan/reddit/urlmanipulation/UrlManipulators.kt
+++ reddit/src/main/java/com/alexsullivan/reddit/urlmanipulation/UrlManipulators.kt
@@ -17,19 +17,19 @@ internal class GfycatUrlManipulator(val logger: Logger): UrlManipulator {
         val gfycatId = item.url.substringAfter(".com/").substringBefore(".")
         val thumbnailId = "http://thumbs.gfycat.com/$gfycatId-poster.jpg"
         return GfycatRepository.create(logger).getImageInfo(gfycatId)
-                .map { item.copy(url = it.mp4, imageType = ImageType.VIDEO, thumbnail = thumbnailId) }
+            .map { item.copy(url = it.mp4, imageType = ImageType.VIDEO, thumbnail = thumbnailId) }
+            .onErrorReturnItem(item)
     }
 }
 
 internal class ImgurUrlManipulator(val logger: Logger): UrlManipulator {
-    override fun matchesDomain(domain: String): Boolean {
-        return domain.contains("imgur.com")
-    }
+    override fun matchesDomain(domain: String) = domain.contains("imgur.com")
 
     override fun modifyRedditItem(item: RedditGifRecipe): Observable<RedditGifRecipe> {
         val imgurId = item.url.substringAfter(".com/").substringBefore(".")
         val thumbnail = "http://i.imgur.com/${imgurId}l.jpg"
         return ImgurRepository.create(logger).getImageInfo(imgurId)
                 .map { item.copy(url = it.mp4, imageType = ImageType.VIDEO, thumbnail = thumbnail) }
+            .onErrorReturnItem(item)
     }
 }
